6f4f25991ef37dfd736180f8975eecf4afb6ab84
Maryam753 <maryemarif89@gmail.com>
2026-06-04 16:30:03 +0500

    fix https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+         if (env('APP_ENV') === 'production') {
+        \URL::forceScheme('https');
+    }
+    
         View::composer('*', function ($view) {
 
         $cartCount = auth()->check()
